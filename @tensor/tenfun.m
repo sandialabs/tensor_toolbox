015a084797dd051a2c394dc4419c52cb8826f83c
Tammy Kolda <tgkolda@sandia.gov>
2017-02-15 17:24:31 -0800

    Completely revamped tenfun. Not can handle an arbitrary binary function.

--- @tensor/tenfun.m
+++ @tensor/tenfun.m
@@ -9,7 +9,7 @@ function Z = tenfun(fun,varargin)
 %   Z = tenfun(@(x)(x+1),X) %<-- increase every element by one
 %   Z = tenfun(@eq,X,1) %<-- logical comparison of X with scalar
 %   Z = tenfun(@plus,X,Y) %<-- adds the two tensors X and Y.
-%   Z = tenfun(@max,X,Y,Z) %<-- max over all elements in X,Y,Z
+%   Z = tenfun(@max,X,Y,Z) %<-- elementwise max over all elements in X,Y,Z
 %
 %   See also TENSOR.
 %
@@ -34,111 +34,67 @@ if ~isa(fun, 'function_handle')
     error('First argument must be a function handle.');
 end
 
-%% Case I: NARGIN == 2 (one function and one tensor)
-if (nargin == 2) && isa(varargin{1},'tensor')
-    Z = varargin{1};
-    Z.data(:) = fun(Z.data(:));
+%% Convert inputs to tensors if they aren't already 
+for i = 1:nargin-1
+    if isscalar(varargin{i}) || isa(varargin{i},'tensor')
+        continue;
+    elseif isnumeric(varargin{i})  
+        varargin{i} = tensor(varargin{i});
+    elseif ismember(class(varargin{i}), {'symtensor','sptensor','ktensor','ttensor'})
+        varargin{i} = full(varargin{i});
+    else
+        error('Invalid input');        
+    end        
+end
+%% It's ok if there are two inputs and one is a scalar; otherwise, all inputs have to be the same size
+if (nargin == 3) && isscalar(varargin{1}) && isa(varargin{2},'tensor')
+    sz = size(varargin{2});
+elseif (nargin == 3) && isscalar(varargin{2}) && isa(varargin{1},'tensor')
+    sz = size(varargin{1});
+else
+    for i = 1:(nargin-1)
+        if isscalar(varargin{i})
+            error('Argument %d is a scalar, but expected a tensor', i+1);
+        elseif i == 1
+            sz = size(varargin{i});
+        elseif ~isequal(sz,size(varargin{i}))
+            error('Tensor %d is not the same size as the first tensor input', i);
+        end
+    end
+end
+
+%% Number of inputs for function handle
+nfunin = nargin(fun);
+
+%% Case I: Binary function
+if (nargin == 3) && (nfunin == 2)   
+    X = varargin{1};
+    Y = varargin{2};    
+    if ~isscalar(X)
+        X = X.data;
+    end
+    if ~isscalar(Y)
+        Y = Y.data;
+    end
+    data = fun(X,Y);
+    Z = tensor(data, sz);
     return;
 end
 
-%% Determine if function is binary 
-% Note that we swap the arguments below if 2nd argument is sparse, but need
-% to take special measures for those functions that don't commute.
-binfuns = {@plus,@minus,@eq,@ge,@gt,@le,@lt,@ne,@and,@or,@xor, ...
-           @power,@times,@ldivide,@rdivide};
-isbinary = false;
-for i = 1 : numel(binfuns)
-    if isequal(fun,binfuns{i})
-        isbinary = true;
-        break;
+
+%% Case II: Expects input to be matrix and applies operation on each column
+if nargin == 2
+    X = varargin{1}.data;
+    X = reshape(X,1,[]);
+else
+    X = zeros(nargin-1,prod(sz));
+    for i = 1:nargin-1
+        X(i,:) = varargin{i}.data(:);
     end
 end
-
-%% Case II: NARGIN == 3 and function is binary
-if (nargin == 3) && isbinary
-    
-    X = varargin{1};
-    Y = varargin{2};
-    
-    % Case IIa: X is a scalar/MDA and Y is a tensor
-    if isnumeric(X) && isa(Y,'tensor')
-        Z = Y;
-        Z.data = fun(X,Y.data);
-        return;
-    end
-
-    % Case IIb: X is a tensor and Y is a scalar/MDA
-    if isa(X,'tensor') && isnumeric(Y)
-        Z = X;
-        Z.data = fun(X.data,Y);
-        return;
-    end
-
-    % Case IIc: X and Y are both tensors
-    if isa(X,'tensor') && isa(Y,'tensor')
-        if ~(isequal(size(X),size(Y)))
-            error('Tensor size mismatch.')
-        end
-        data = fun(X.data,Y.data);
-        Z = tensor(data,size(X));
-        return;
-    end
-    
-    % Case IId: Either X or Y is a sptensor
-    if isa(X,'tensor') && isa(Y,'sptensor')
-        if isequal(fun,@lt)
-            Z = gt(Y,X);
-        elseif isequal(fun,@le)
-            Z = ge(Y,X);
-        elseif isequal(fun,@gt)
-            Z = lt(Y,X);
-        elseif isequal(fun,@ge)
-            Z = le(Y,X);
-        elseif isequal(fun,@minus)
-            Z = plus(-Y,X);
-        elseif isequal(fun,@power)
-            error('Cannot do array power with a sparse and dense tensor');
-        elseif isequal(fun,@ldivide)
-            error('Cannot do ldivide with a sparse and dense tensor');
-        elseif isequal(fun,@rdivide)
-            error('Cannot do rdivide with a sparse and dense tensor');
-        else
-            Z = fun(Y,X);
-        end
-        return;
-    end
-    
-    % Case IIe: If Y is a symtensor
-    if isa(X,'tensor') && isa(Y,'symtensor')
-            Z = fun(full(Y),X);
-        return;
-    end
-    
-    % Case IIf: Either X or Y is not a tensor   
-    error('For a binary function, arguments must be either two tensors or one tensor and a scalar/MDA.');
-end
-
-%% Case III: one function and the rest are same-sized tensors
-X = varargin;  %A cell array of inputted tensors
-n = numel(X);
-sz = size(X{1});
-
-for i=2:n  %Verify that tensors are same size
-    if ~isequal(size(X{i}),sz)
-        error('Tensors are not same size');
-    end
-end
-
-m = prod(sz);   %Number of elements in X.data
-Y = zeros(m,n);  %An array of linearized X.data arrays. 
-for j = 1:n
-    Y(:,j) = X{j}.data(:);
-end
-data = zeros(m,1);
-for i = 1:m
-    data(i) = fun(Y(i,:)); 
-end
+data = fun(X);
 Z = tensor(data,sz);
 return;
+
 
 
