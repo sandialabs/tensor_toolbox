773856c59d06cedb18089560aeb07154002e10b6
David Hong <dhong@sandia.gov>
2017-05-22 14:18:27 -0700

    Added some more comments and slightly reorganizing.

--- @ktensor/fg_est.m
+++ @ktensor/fg_est.m
@@ -1,30 +1,34 @@
 function [fest,Gest,info] = fg_est(M, X, subs, varargin)
-%FG_EST Estimate mean element function value from a few samples
+%FG_EST Objective and gradient function estimation for fitting Ktensor to
+%data. Estimation is done by using a few samples of the data rather than
+%the full data.
 
-%%
+%% Extract problem dimensions
+d = ndims(X);
+n = size(X);
+r = ncomponents(M);
+
+%% Set algorithm parameters from input or by using defaults
 params = inputParser;
 params.addParameter('xvals',[]);
 params.addParameter('Uexplode',[]);
 params.parse(varargin{:});
 
-%%
-d = ndims(X);
-n = size(X);
-r = ncomponents(M);
+%% Copy from params object
+xvals    = params.Results.xvals;
+Uexplode = params.Results.Uexplode;
 
-%% Extra entries specified by subs
-xvals = params.Results.xvals;
+%% Extract entries specified by subs
 if isempty(xvals)
     xvals = X(subs);
 end
 
-%% Grab appropriate rows of each factor matrix
-Uexplode = params.Results.Uexplode;
+%% Extract appropriate rows of each factor matrix
 if isempty(Uexplode)
     Uexplode = cell(d,1);
     for k = 1:d
         Uexplode{k} = M.u{k}(subs(:,k),:);
-    end 
+    end
 end
 
 %% Calculate model values
@@ -36,10 +40,10 @@ for k = 2:d
 end
 mvals = sum(tmp,2);
 
-%% Compute function value (and scale)
+%% Compute mean function value and scale
 fest = mean( (xvals - mvals).^2 ) * prod(n);
 
-%% Compute gradient 
+%% Compute gradient and scale
 % This is inherently doing an MTTKRP, but we avoid forming the G tensor or
 % calling the MTTKRP function. THis is probably inefficient and also
 % ignores the lambdas (i.e., assumes they are one).
@@ -49,12 +53,12 @@ KRexplode = cell(size(M.u));
 for k = 1:d
     KRexplode{k} = ones(size(Uexplode{1}));
 end
-for k = 1:d 
+for k = 1:d
     for kalt = 1:d
         if k ~= kalt
             KRexplode{kalt} = KRexplode{kalt} .* Uexplode{k};
         end
-    end    
+    end
 end
 
 Gest = cell(d,1);
@@ -67,8 +71,7 @@ for k = 1:d
     end
     Gest{k} = gm / size(subs,1) * prod(n);
 end
-    
 
-%%
+%% Output potentially reused values
 info.xvals = xvals;
 info.Uexplode = Uexplode;
