3456d1495c457da715009ff75b14a9895c8117a9
David Hong <dhong@sandia.gov>
2017-08-11 18:14:10 -0700

    Add some documentation.

--- cp_adam_all.m
+++ cp_adam_all.m
@@ -1,4 +1,7 @@
 function [subs,wvals,meta] = cp_adam_all(~,sz,mask,meta)
+%CP_ADAM_ALL Subindices to sample all known entries of the tensor.
+%
+%   See also CP_ADAM.
 
 if ~isfield(meta,'allsubs')
     if isempty(mask) % No missing entries
--- cp_adam_prop.m
+++ cp_adam_prop.m
@@ -1,9 +1,24 @@
-function [subs,wvals,meta] = cp_adam_prop(nsamples,sz,mask,meta,X,offset)
+function [subs,wvals,meta] = cp_adam_prop(nsample,sz,mask,meta,X,offset)
+%CP_ADAM_PROP Subindices to sample nsample entries of the tensor sampled
+%with probability proportional to the product of the marginal sums (with an
+%added offset)
+%
+%   Namely, entry (i,j,k) of a 3-way tensor X is sampled with probability
+%   proportional to
+%
+%     collapse(X(i,:,:))*collapse(X(:,j,:))*collapse(X(:,:,k)) + offset
+%
+%   Offset can be used to make the probabilities generally more balanced.
+%
+%   wvals provides inverse probabilities that can be used in a weighted sum
+%   to provide an unbiased estimate of the unweighted sum.
+%
+%   See also CP_ADAM.
 
 % Only handle fully-sampled data for now
 if ~isempty(mask) && ~isfield(meta,'prop_missing_flag')
     warning('Proportional sampling not yet supported for missing data. Using uniform.');
-    [subs,wvals,meta] = cp_adam_unif(nsamples,sz,mask,meta);
+    [subs,wvals,meta] = cp_adam_unif(nsample,sz,mask,meta);
     meta.prop_missing_flag = true;
     return;
 end
@@ -21,10 +36,10 @@ if ~isfield(meta,'cprobs')
     meta.cprobs = cellfun(@(p) [0; cumsum(p(1:end-1)); 1],meta.probs,'UniformOutput',false);
 end
 
-subs = zeros(nsamples,n);
-wvals = ones(nsamples,1);
+subs = zeros(nsample,n);
+wvals = ones(nsample,1);
 for k = 1:n
-    [~,~,subs(:,k)] = histcounts(rand(nsamples,1),meta.cprobs{k});
+    [~,~,subs(:,k)] = histcounts(rand(nsample,1),meta.cprobs{k});
     wvals = wvals./meta.probs{k}(subs(:,k));
 end
 
