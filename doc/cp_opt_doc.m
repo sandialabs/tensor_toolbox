d3bb0ef332d6da5f2805582e8e0ce9dc077b11cc
Tamara G. Kolda <tgkolda@sandia.gov>
2017-11-20 15:04:20 -0800

    Adding option to use L-BFGS-B code by Stephen Becker (which is really a translation of famous Fortran code). This is now the default for cp_opt and cp_wopt. This adds the ability to use bound constraints, which is a major enhancement!!

--- doc/cp_opt_doc.m
+++ doc/cp_opt_doc.m
@@ -1,16 +1,33 @@
 %% All-at-once optimization for CP tensor decomposition
-% We explain how to use |cp_opt| with the Poblano toolbox.
-% The method is described in the following reference:
+% We explain how to use |cp_opt| function which implements the *CP-OPT*
+% method that fits the CP model using _direct_ or _all-at-once_
+% optimization. This is in contrast to the |cp_als| function which
+% implements the *CP-ALS* that fits the CP model using _alternating_ 
+% optimization. The CP-OPT method is described in the
+% following reference: 
 %
-% E. Acar, D. M. Dunlavy and T. G. Kolda, A Scalable
-% Optimization Approach for Fitting Canonical Tensor Decompositions,
-% J. Chemometrics 25(2):67-86, February 2011 (doi:10.1002/cem.1335)
+% * E. Acar, D. M. Dunlavy and T. G. Kolda, *A Scalable
+% Optimization Approach for Fitting Canonical Tensor Decompositions*,
+% _J. Chemometrics_ 25(2):67-86, February 2011
+% <http://doi.org/10.1002/cem.1335 (doi:10.1002/cem.1335)>
 
 
-%% Poblano Optimization Toolbox
-% Check that you have Poblano 1.1 installed. The output of your 'ver'
-% command should look something like the following.
-ver
+%% Third-party optimization software
+% The |cp_opt| method uses third-party optimization software to do the
+% optimization. You can use either 
+%
+% * <https://github.com/stephenbeckr/L-BFGS-B-C *L-BFGS-B* by Stephen Becker> 
+% (preferred), or
+% * <https://software.sandia.gov/trac/poblano *POBLANO* Version 1.1 by
+% Evrim Acar, Daniel Dunlavy, and Tamara Kolda>.
+%
+% The remainder of these instructions assume L-BFGS-B is being used. See
+% <cp_opt_poblano_doc.html here> for instructions on using |cp_opt| with
+% Poblano.
+
+%% Check that the software is installed. 
+% Be sure that lbfgsb is in your path.
+help lbfgsb
 
 %% Create an example problem. 
 % Create an example 50 x 40 x 30 tensor with rank 5 and add 10% noise.
@@ -20,45 +37,21 @@ X = info.Data;
 M_true = info.Soln;
 
 %% Create initial guess using 'nvecs'
-M_init = create_guess('Data', X, 'Num_Factors', R, ...
-    'Factor_Generator', 'nvecs');
-
-
-%% Set up the optimization parameters
-% It's genearlly a good idea to consider the parameters of the optimization
-% method. The default options may be either too stringent or not stringent
-% enough. The most important options to consider are detailed here. 
-
-% Get the defaults
-ncg_opts = ncg('defaults');
-% Tighten the stop tolerance (norm of gradient). This is often too large.
-ncg_opts.StopTol = 1.0e-6;
-% Tighten relative change in function value tolearnce. This is often too large.
-ncg_opts.RelFuncTol = 1.0e-20;
-% Increase the number of iterations. 
-ncg_opts.MaxIters = 10^4;
-% Only display every 10th iteration
-ncg_opts.DisplayIters = 10;
-% Display the final set of options
-ncg_opts
+M_init = create_guess('Data', X, 'Num_Factors', R, 'Factor_Generator', 'nvecs');
 
 %% Call the |cp_opt| method
 % Here is an example call to the cp_opt method. By default, each iteration
 % prints the least squares fit function value (being minimized) and the
-% norm of the gradient. The meaning of any line search warnings
-% can be checked via <matlab:doc('cvsrch') doc cvsrch>.
-[M,~,output] = cp_opt(X, R, 'init', M_init, ...
-    'alg', 'ncg', 'alg_options', ncg_opts);
+% norm of the gradient. 
+
+[M,M0,output] = cp_opt(X, R, 'init', M_init);
 
 %% Check the output
 % It's important to check the output of the optimization method. In
-% particular, it's worthwhile to check the exit flag. 
-% A zero (0) indicates successful termination with the gradient smaller
-% than the specified StopTol, and a three (3) indicates a successful
-% termination where the change in function value is less than RelFuncTol.
-% The meaning of any other flags can be checked via 
-% <matlab:doc('poblano_params') doc poblano_params>. 
-exitflag = output.ExitFlag
+% particular, it's worthwhile to check the exit message. 
+% The message |CONVERGENCE: REL_REDUCTION_OF_F_<=_FACTR*EPSMCH| means that
+% it has converged because the function value stopped improving.
+exitmsg = output.ExitMsg
 
 %%
 % The fit is the percentage of the data that is explained by the model.
@@ -74,29 +67,59 @@ fit = output.Fit
 scr = score(M,M_true)
 
 %% Overfitting example
-% Consider the case where we don't know R in advance. We might guess too
-% high. Here we show a case where we guess R+1 factors rather than R.
+% Re-using the same example as before, consider the case where we don't
+% know R in advance. We might guess too high. Here we show a case where we
+% guess R+1 factors rather than R. 
 
 % Generate initial guess of the corret size
 M_plus_init = create_guess('Data', X, 'Num_Factors', R+1, ...
     'Factor_Generator', 'nvecs');
 
-%% 
-
-% Loosen the stop tolerance (norm of gradient). 
-ncg_opts.StopTol = 1.0e-2;
-
 %%
 
 % Run the algorithm
-[M_plus,~,output] = cp_opt(X, R+1, 'init', M_plus_init, ...
-    'alg', 'ncg', 'alg_options', ncg_opts);
-exitflag = output.ExitFlag
+[M_plus,~,output] = cp_opt(X, R+1, 'init', M_plus_init);
+exitmsg = output.ExitMsg
 fit = output.Fit
-
 
 %%
 
 % Check the answer (1 is perfect)
 scr = score(M_plus, M_true)
 
+%% Nonnegative factorization
+% We can employ lower bounds to get a nonnegative factorization.
+
+%% Create an example problem. 
+% Create an example 50 x 40 x 30 tensor with rank 5 and add 10% noise. We
+% select nonnegative factor matrices and lambdas. The
+% create_problem doesn't really know how to add noise without going
+% negative, so we _hack_ it to make the observed tensor be nonzero.
+R = 5;
+info = create_problem('Size', [50 40 30], 'Num_Factors', R, 'Noise', 0.10,...
+    'Factor_Generator', 'rand', 'Lambda_Generator', 'rand');
+X = info.Data .* (info.Data > 0); % Force it to be nonnegative
+M_true = info.Soln;
+
+%% Generate initial guess of the corret size
+M_init = create_guess('Data', X, 'Num_Factors', R, ...
+    'Factor_Generator', 'rand');
+%% Call the |cp_opt| method
+% Here we specify a lower bound of zero with the last two arguments.
+[M,M0,output] = cp_opt(X, R, 'init', M_init,'lower',0);
+
+%% Check the output
+exitmsg = output.ExitMsg
+
+%%
+% The fit is the percentage of the data that is explained by the model.
+% Because we have noise, we do not expect the fit to be perfect.
+fit = output.Fit
+
+%% Evaluate the output
+% We can "score" the similarity of the model computed by CP and compare
+% that with the truth. The |score| function on ktensor's gives a score in
+% [0,1]  with 1 indicating a perfect match. Because we have noise, we do
+% not expect the fit to be perfect. See <matlab:doc('ktensor/score') doc
+% score> for more details.
+scr = score(M,M_true)
--- doc/cp_wopt_doc.m
+++ doc/cp_wopt_doc.m
@@ -1,11 +1,24 @@
 %% Weighted optimization for CP tensor decomposition with incomplete data
-% We explain how to use |cp_wopt| with the POBLANO toolbox. The method is
-% described in the following article:
+% We explain how to use the CP Weighted Optimization (CP-WOPR) method
+% implements in |cp_wopt|. The method is described in the following article:
 %
-% E. Acar, D. M. Dunlavy, T. G. Kolda and M. Mørup, 
-% Scalable Tensor Factorizations for Incomplete Data, 
-% Chemometrics and Intelligent Laboratory Systems 106(1):41-56, March 2011
+% * E. Acar, D. M. Dunlavy, T. G. Kolda and M. Mørup, 
+% *Scalable Tensor Factorizations for Incomplete Data*, 
+% _Chemometrics and Intelligent Laboratory Systems_ 106(1):41-56, March 2011
 % (doi:10.1016/j.chemolab.2010.08.004) 
+
+%% Third-party optimization software
+% The |cp_wopt| method uses third-party optimization software to do the
+% optimization. You can use either 
+%
+% * <https://github.com/stephenbeckr/L-BFGS-B-C *L-BFGS-B* by Stephen Becker> 
+% (preferred), or
+% * <https://software.sandia.gov/trac/poblano *POBLANO* Version 1.1 by
+% Evrim Acar, Daniel Dunlavy, and Tamara Kolda>.
+%
+% The remainder of these instructions assume L-BFGS-B is being used. See
+% <cp_wopt_poblano_doc.html here> for instructions on using |cp_wopt| with
+% Poblano.
 
 %% Important Information
 % 
@@ -27,41 +40,20 @@ M_init = create_guess('Data', X, 'Num_Factors', R, ...
     'Factor_Generator', 'nvecs');
 
 
-%% Set up the optimization parameters
-% It's genearlly a good idea to consider the parameters of the optimization
-% method. The default options may be either too stringent or not stringent
-% enough. The most important options to consider are detailed here. 
 
-% Get the defaults
-ncg_opts = ncg('defaults');
-% Tighten the stop tolerance (norm of gradient). This is often too large.
-ncg_opts.StopTol = 1.0e-6;
-% Tighten relative change in function value tolearnce. This is often too large.
-ncg_opts.RelFuncTol = 1.0e-20;
-% Increase the number of iterations. 
-ncg_opts.MaxIters = 10^4;
-% Only display every 10th iteration
-ncg_opts.DisplayIters = 10;
-% Display the final set of options
-ncg_opts
 
 %% Call the |cp_wopt| method
 % Here is an example call to the cp_opt method. By default, each iteration
 % prints the least squares fit function value (being minimized) and the
-% norm of the gradient. The meaning of any line search warnings
-% can be checked via <matlab:doc('cvsrch') doc cvsrch>.
-[M,~,output] = cp_wopt(X, P, R, 'init', M_init, ...
-    'alg', 'ncg', 'alg_options', ncg_opts);
+% norm of the gradient. 
+[M,~,output] = cp_wopt(X, P, R, 'init', M_init);
 
 %% Check the output
 % It's important to check the output of the optimization method. In
-% particular, it's worthwhile to check the exit flag. 
-% A zero (0) indicates successful termination with the gradient smaller
-% than the specified StopTol, and a three (3) indicates a successful
-% termination where the change in function value is less than RelFuncTol.
-% The meaning of any other flags can be checked via 
-% <matlab:doc('poblano_params') doc poblano_params>. 
-exitflag = output.ExitFlag
+% particular, it's worthwhile to check the exit message for any problems.
+% The message |CONVERGENCE: REL_REDUCTION_OF_F_<=_FACTR*EPSMCH| means that
+% it has converged because the function value stopped improving.
+exitmsg = output.ExitMsg
 
 
 %% Evaluate the output
@@ -86,48 +78,13 @@ M_init = create_guess('Data', X, 'Num_Factors', R, ...
     'Factor_Generator', 'nvecs');
 
 
-%% Set up the optimization parameters
-% It's genearlly a good idea to consider the parameters of the optimization
-% method. The default options may be either too stringent or not stringent
-% enough. The most important options to consider are detailed here. 
-
-% Get the defaults
-ncg_opts = ncg('defaults');
-% Tighten the stop tolerance (norm of gradient). This is often too large.
-ncg_opts.StopTol = 1.0e-6;
-% Tighten relative change in function value tolearnce. This is often too large.
-ncg_opts.RelFuncTol = 1.0e-20;
-% Increase the number of iterations. 
-ncg_opts.MaxIters = 10^4;
-% Only display every 10th iteration
-ncg_opts.DisplayIters = 10;
-% Display the final set of options
-ncg_opts
-
 %% Call the |cp_wopt| method
-% Here is an example call to the cp_opt method. By default, each iteration
-% prints the least squares fit function value (being minimized) and the
-% norm of the gradient. The meaning of any line search warnings
-% can be checked via <matlab:doc('cvsrch') doc cvsrch>.
-[M,~,output] = cp_wopt(X, P, R, 'init', M_init, ...
-    'alg', 'ncg', 'alg_options', ncg_opts);
+[M,~,output] = cp_wopt(X, P, R, 'init', M_init);
 
 %% Check the output
-% It's important to check the output of the optimization method. In
-% particular, it's worthwhile to check the exit flag. 
-% A zero (0) indicates successful termination with the gradient smaller
-% than the specified StopTol, and a three (3) indicates a successful
-% termination where the change in function value is less than RelFuncTol.
-% The meaning of any other flags can be checked via 
-% <matlab:doc('poblano_params') doc poblano_params>. 
-exitflag = output.ExitFlag
+exitmsg = output.ExitMsg
 
 
 %% Evaluate the output
-% We can "score" the similarity of the model computed by CP and compare
-% that with the truth. The |score| function on ktensor's gives a score in
-% [0,1]  with 1 indicating a perfect match. Because we have noise, we do
-% not expect the fit to be perfect. See <matlab:doc('ktensor/score') doc
-% score> for more details.
 scr = score(M,M_true)
 
